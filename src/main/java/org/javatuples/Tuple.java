99549d0505007d1acf751b5a60b836773dbb225f
Daniel Fernández <daniel.fernandez@11thlabs.org>
2010-10-19 22:53:44 +0000

    Several changes made
    git-svn-id: https://java-tuples.svn.sourceforge.net/svnroot/java-tuples/trunk/javatuples@6 489fede1-5133-420c-9a1d-d78e41d428e2

--- src/main/java/org/javatuples/Tuple.java
+++ src/main/java/org/javatuples/Tuple.java
@@ -108,5 +108,36 @@ public abstract class Tuple implements Iterable<Object>, Serializable {
         }
         return false;
     }
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((this.valueList == null) ? 0 : this.valueList.hashCode());
+        return result;
+    }
+
+
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tuple other = (Tuple) obj;
+        return this.valueList.equals(other.valueList);
+    }
+    
+    
+    
     
 }
